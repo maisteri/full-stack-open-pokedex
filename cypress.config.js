60a47411cb96502653af3666650b60a9cac885d4
Atte Kivistö <atte.t.kivisto@gmail.com>
2023-05-12 08:52:02 +0300

    cypress linting errors fixed

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    // eslint-disable-next-line no-unused-vars
+    setupNodeEvents(_on, _config) {
       // implement node event listeners here
     },
   },
